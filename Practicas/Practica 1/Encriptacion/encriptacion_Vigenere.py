4c617b739f6eb5e6f9d66307c73489a606376fa3
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-09-20 12:36:33 +0200

    añadidos los ejercicios hechos en casa
    añadidos los ejercicios cifrado_AES y ud2_1

--- Practicas/Practica 1/Encriptacion/encriptacion_Vigenere.py
+++ Practicas/Practica 1/Encriptacion/encriptacion_Vigenere.py
@@ -1,3 +1,5 @@
+#Cifrado del texto
+
 #Elegir la clave
 clave="sorcerer"
 clave=clave.upper()
@@ -7,7 +9,39 @@ clave=clave.upper()
 archivo=open("carpeta/pako.txt","r")
 textoParaCifrar=""
 for i in archivo:
-    i.upper()
+    i=i.upper()
     for j in range(len(i)):
         if ord(i[j])>=ord("A") and ord(i[j])<=ord("Z"):
-            print("Hola")
+            textoParaCifrar=textoParaCifrar+i[j]
+archivo.close()
+
+#Fijar el tamanio de la clave para que sea igual que el de textoParaCifrar o
+#superior en caso de que la clave original fuera mas grande que el 
+#textoParaCifrar
+claveUsar=""
+if(len(clave)<len(textoParaCifrar)):
+    for i in range(len(textoParaCifrar)):
+        if(i>=len(clave)):
+            letra=i%len(clave)
+        else:
+            letra=i
+        claveUsar=claveUsar+clave[letra]
+else:
+    claveUsar=clave
+
+#Cifrar el texto con la suma de las letras
+textoCifrado=""
+for i in range(len(textoParaCifrar)):
+    numLetra=ord(textoParaCifrar[i])+ord(claveUsar[i])-ord("A")-ord("A")
+    letra=chr(numLetra%26+ord("A"))
+    textoCifrado=textoCifrado+letra
+print(f"El texto cifrado es: {textoCifrado}")
+#Hasta aqui el cifrado
+
+#Descifrar
+textoDescifrado=""
+for i in range(len(textoCifrado)):
+    numLetra=ord(textoCifrado[i])-ord(claveUsar[i])-ord("A")-ord("A")
+    letra=chr((numLetra+26)%26+ord("A"))
+    textoDescifrado=textoDescifrado+letra
+print(f"El texto descifrado es: {textoDescifrado}")
